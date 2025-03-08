54cf3aee5cc9ee79018b1ec4bfd955bcc0e5b374
85923 <xtls@jlhs.me>
2025-03-08 19:12:54 +1300

    Update socks.go

--- operation/protocol/tcp/socks.go
+++ operation/protocol/tcp/socks.go
@@ -3,8 +3,8 @@ package tcp
 import (
 	"net"
 
-	"github.com/uQUIC/XGFW/operation/protocol"
-	"github.com/uQUIC/XGFW/operation/protocol/utils"
+	"github.com/v2TLS/XGFW/operation/protocol"
+	"github.com/v2TLS/XGFW/operation/protocol/utils"
 )
 
 const (
