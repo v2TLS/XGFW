bb657cbba906b6b31f9279536b3d59048ce21cd9
oweid <QUIC-Go@jlhs.me>
2025-04-26 21:10:48 +0800

    Update handshake.go

--- operation/filter/tcp/handshake.go
+++ operation/filter/tcp/handshake.go
@@ -7,9 +7,9 @@ import (
     "sync"
     "time"
 
-    "github.com/v2TLS/XGFW/operation/protocol"
-    "github.com/v2TLS/XGFW/operation/protocol/internal"
-    "github.com/v2TLS/XGFW/operation/protocol/utils"
+    "github.com/v2TLS/XGFW/operation/filter"
+    "github.com/v2TLS/XGFW/operation/filter/internal"
+    "github.com/v2TLS/XGFW/operation/filter/utils"
 )
 
 // 确保实现接口
