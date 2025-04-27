c1196852be4475c2fc545fe1ce53442a33381f28
oweid <QUIC-Go@jlhs.me>
2025-04-27 17:48:28 +0800

    Update dns.go

--- operation/protocol/udp/dns.go
+++ operation/protocol/udp/dns.go
@@ -1,7 +1,7 @@
 package udp
 
 import (
-	"github.com/v2TLS/XGFW/operation/protocol"
+	"github.com/v2TLS/XGFW/operation"
 	"github.com/v2TLS/XGFW/operation/protocol/utils"
 
 	"github.com/google/gopacket"
