30b6b5c62aedd60331e4464b515b3b5dff6695ff
oweid <QUIC-Go@jlhs.me>
2025-05-10 12:59:43 +0800

    feat: enable resp for QUIC

--- operation/protocol/udp/quic.go
+++ operation/protocol/udp/quic.go
@@ -33,49 +33,141 @@ func (a *QUICAnalyzer) NewUDP(info analyzer.UDPInfo, logger analyzer.Logger) ana
 type quicStream struct {
 	logger       analyzer.Logger
 	invalidCount int
+
+	reqBuf     *utils.ByteBuffer
+	reqMap     analyzer.PropMap
+	reqUpdated bool
+	reqLSM     *utils.LinearStateMachine
+	reqDone    bool
+
+	respBuf     *utils.ByteBuffer
+	respMap     analyzer.PropMap
+	respUpdated bool
+	respLSM     *utils.LinearStateMachine
+	respDone    bool
+
+	clientHelloLen int
+	serverHelloLen int
 }
 
 func (s *quicStream) Feed(rev bool, data []byte) (u *analyzer.PropUpdate, done bool) {
-	// minimal data size: protocol version (2 bytes) + random (32 bytes) +
-	//   + session ID (1 byte) + cipher suites (4 bytes) +
-	//   + compression methods (2 bytes) + no extensions
-	const minDataSize = 41
-
+	if s.reqBuf == nil {
+		s.reqBuf = &utils.ByteBuffer{}
+		s.respBuf = &utils.ByteBuffer{}
+		s.reqLSM = utils.NewLinearStateMachine(
+			s.quicClientHelloPreprocess,
+			s.parseClientHelloData,
+		)
+		s.respLSM = utils.NewLinearStateMachine(
+			s.quicServerHelloPreprocess,
+			s.parseServerHelloData,
+		)
+	}
+	if len(data) == 0 {
+		return nil, false
+	}
+	var update *analyzer.PropUpdate
+	var cancelled bool
 	if rev {
-		// We don't support server direction for now
-		s.invalidCount++
-		return nil, s.invalidCount >= quicInvalidCountThreshold
+		s.respBuf.Append(data)
+		s.respUpdated = false
+		cancelled, s.respDone = s.respLSM.Run()
+		if s.respUpdated {
+			update = &analyzer.PropUpdate{
+				Type: analyzer.PropUpdateMerge,
+				M:    analyzer.PropMap{"resp": s.respMap},
+			}
+			s.respUpdated = false
+		}
+	} else {
+		s.reqBuf.Append(data)
+		s.reqUpdated = false
+		cancelled, s.reqDone = s.reqLSM.Run()
+		if s.reqUpdated {
+			update = &analyzer.PropUpdate{
+				Type: analyzer.PropUpdateMerge,
+				M:    analyzer.PropMap{"req": s.reqMap},
+			}
+			s.reqUpdated = false
+		}
 	}
+	return update, cancelled || (s.reqDone && s.respDone)
+}
 
-	pl, err := quic.ReadCryptoPayload(data)
-	if err != nil || len(pl) < 4 { // FIXME: isn't length checked inside quic.ReadCryptoPayload? Also, what about error handling?
-		s.invalidCount++
-		return nil, s.invalidCount >= quicInvalidCountThreshold
+// ClientHello preprocess: same logic as TLS for handshake validation
+func (s *quicStream) quicClientHelloPreprocess() utils.LSMAction {
+	const headersSize = 4 // Type(1) + Len(3)
+	const minDataSize = 41
+	header, ok := s.reqBuf.Get(headersSize, true)
+	if !ok {
+		return utils.LSMActionPause
 	}
-
-	if pl[0] != internal.TypeClientHello {
-		s.invalidCount++
-		return nil, s.invalidCount >= quicInvalidCountThreshold
+	if header[0] != internal.TypeClientHello {
+		return utils.LSMActionCancel
 	}
-
-	chLen := int(pl[1])<<16 | int(pl[2])<<8 | int(pl[3])
-	if chLen < minDataSize {
-		s.invalidCount++
-		return nil, s.invalidCount >= quicInvalidCountThreshold
+	s.clientHelloLen = int(header[1])<<16 | int(header[2])<<8 | int(header[3])
+	if s.clientHelloLen < minDataSize {
+		return utils.LSMActionCancel
 	}
+	return utils.LSMActionNext
+}
 
-	m := internal.ParseTLSClientHelloMsgData(&utils.ByteBuffer{Buf: pl[4:]})
+// ServerHello preprocess: same logic as TLS for handshake validation
+func (s *quicStream) quicServerHelloPreprocess() utils.LSMAction {
+	const headersSize = 4 // Type(1) + Len(3)
+	const minDataSize = 38
+	header, ok := s.respBuf.Get(headersSize, true)
+	if !ok {
+		return utils.LSMActionPause
+	}
+	if header[0] != internal.TypeServerHello {
+		return utils.LSMActionCancel
+	}
+	s.serverHelloLen = int(header[1])<<16 | int(header[2])<<8 | int(header[3])
+	if s.serverHelloLen < minDataSize {
+		return utils.LSMActionCancel
+	}
+	return utils.LSMActionNext
+}
+
+func (s *quicStream) parseClientHelloData() utils.LSMAction {
+	chBuf, ok := s.reqBuf.GetSubBuffer(s.clientHelloLen, true)
+	if !ok {
+		return utils.LSMActionPause
+	}
+	m := internal.ParseTLSClientHelloMsgData(chBuf)
 	if m == nil {
-		s.invalidCount++
-		return nil, s.invalidCount >= quicInvalidCountThreshold
+		return utils.LSMActionCancel
+	} else {
+		s.reqUpdated = true
+		s.reqMap = m
+		return utils.LSMActionNext
 	}
+}
 
-	return &analyzer.PropUpdate{
-		Type: analyzer.PropUpdateMerge,
-		M:    analyzer.PropMap{"req": m},
-	}, true
+func (s *quicStream) parseServerHelloData() utils.LSMAction {
+	shBuf, ok := s.respBuf.GetSubBuffer(s.serverHelloLen, true)
+	if !ok {
+		return utils.LSMActionPause
+	}
+	m := internal.ParseTLSServerHelloMsgData(shBuf)
+	if m == nil {
+		return utils.LSMActionCancel
+	} else {
+		s.respUpdated = true
+		s.respMap = m
+		return utils.LSMActionNext
+	}
 }
 
 func (s *quicStream) Close(limited bool) *analyzer.PropUpdate {
+	if s.reqBuf != nil {
+		s.reqBuf.Reset()
+	}
+	if s.respBuf != nil {
+		s.respBuf.Reset()
+	}
+	s.reqMap = nil
+	s.respMap = nil
 	return nil
 }
