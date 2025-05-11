0b0e42d0912358cff7ccd0a60af2cf1ddd0b308f
oweid <QUIC-Go@jlhs.me>
2025-05-11 11:29:49 +0800

    Update dynamic_route.go

--- modifier/udp/dynamic_route.go
+++ modifier/udp/dynamic_route.go
@@ -1,13 +1,11 @@
-package udp
+package modifier
 
 import (
 	"errors"
 	"math/rand"
 	"net"
-	"time"
 	"strconv"
-
-	"github.com/v2TLS/XGFW/modifier"
+	"time"
 )
 
 // GatewayConfig 定义每个出口网关的配置
@@ -24,10 +22,10 @@ func (m *DynamicRouteModifier) Name() string {
 	return "dynamic_route"
 }
 
-func (m *DynamicRouteModifier) New(args map[string]interface{}) (modifier.Instance, error) {
+func (m *DynamicRouteModifier) New(args map[string]interface{}) (Instance, error) {
 	gs, ok := args["gateways"].([]interface{})
 	if !ok || len(gs) == 0 {
-		return nil, &modifier.ErrInvalidArgs{Err: errors.New("missing gateways")}
+		return nil, &ErrInvalidArgs{Err: errors.New("missing gateways")}
 	}
 	var gateways []GatewayConfig
 	total := 0
@@ -53,7 +51,7 @@ func (m *DynamicRouteModifier) New(args map[string]interface{}) (modifier.Instan
 		total += percent
 	}
 	if total != 100 {
-		return nil, &modifier.ErrInvalidArgs{Err: errors.New("total percent must be 100")}
+		return nil, &ErrInvalidArgs{Err: errors.New("total percent must be 100")}
 	}
 	return &dynamicRouteInstance{gateways: gateways, randSrc: rand.New(rand.NewSource(time.Now().UnixNano()))}, nil
 }
@@ -63,7 +61,7 @@ type dynamicRouteInstance struct {
 	randSrc  *rand.Rand
 }
 
-// Process: 按百分比选择网关，转发UDP包。如果IP为0.0.0.0则表示直接放行（不修改数据包）。
+// UDP实现
 func (i *dynamicRouteInstance) Process(data []byte) ([]byte, error) {
 	idx := i.pickGatewayIndex()
 	gw := i.gateways[idx]
@@ -71,22 +69,40 @@ func (i *dynamicRouteInstance) Process(data []byte) ([]byte, error) {
 		// 0.0.0.0 表示直接放行
 		return data, nil
 	}
-	// 直接UDP发送到目标网关
 	addr := net.JoinHostPort(gw.IP, strconv.Itoa(gw.Port))
 	conn, err := net.Dial("udp", addr)
 	if err != nil {
-		return nil, &modifier.ErrInvalidPacket{Err: err}
+		return nil, &ErrInvalidPacket{Err: err}
 	}
 	defer conn.Close()
 	_, err = conn.Write(data)
 	if err != nil {
-		return nil, &modifier.ErrInvalidPacket{Err: err}
+		return nil, &ErrInvalidPacket{Err: err}
 	}
 	// 返回空，表示本地不再转发到公网
 	return nil, nil
 }
 
-// pickGatewayIndex 按百分比随机选一个网关
+// TCP实现
+func (i *dynamicRouteInstance) ProcessTCP(data []byte, direction bool) ([]byte, error) {
+	idx := i.pickGatewayIndex()
+	gw := i.gateways[idx]
+	if gw.IP == "0.0.0.0" || gw.IP == "" {
+		return data, nil
+	}
+	addr := net.JoinHostPort(gw.IP, strconv.Itoa(gw.Port))
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		return nil, &ErrInvalidPacket{Err: err}
+	}
+	defer conn.Close()
+	_, err = conn.Write(data)
+	if err != nil {
+		return nil, &ErrInvalidPacket{Err: err}
+	}
+	return nil, nil
+}
+
 func (i *dynamicRouteInstance) pickGatewayIndex() int {
 	r := i.randSrc.Intn(100)
 	acc := 0
@@ -99,6 +115,6 @@ func (i *dynamicRouteInstance) pickGatewayIndex() int {
 	return len(i.gateways) - 1
 }
 
-// 确保接口实现
-var _ modifier.Modifier = (*DynamicRouteModifier)(nil)
-var _ modifier.UDPModifierInstance = (*dynamicRouteInstance)(nil)
+var _ Modifier = (*DynamicRouteModifier)(nil)
+var _ UDPModifierInstance = (*dynamicRouteInstance)(nil)
+var _ TCPModifierInstance = (*dynamicRouteInstance)(nil)
